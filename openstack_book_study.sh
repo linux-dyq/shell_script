64942b32cb32e75f48789f50b9550c686203cafc
linux-dyq <58285734+linux-dyq@users.noreply.github.com>
2021-01-26 00:01:40 -0800

    Create openstack_book_study.sh

---
+++ openstack_book_study.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# 2021-1-26 15:57:55
+# 学习openstack书
+openstack-keystone-2012.2.4-5.el6.noarch.rpm
+/etc/keystone
+/etc/keystone/default_catalog.templates
+/etc/keystone/keystone.conf
+/etc/keystone/logging.conf
+/etc/keystone/plolicy.json
+/etc/logrotate.d/openstack-keystone
+/etc/rc.d/init.d/openstack-keystone
+/usr/bin/keystone-all
+/usr/bin/keystone-manage
+/usr/bin/openstack-keystone-sample-date
+/usr/share/doc/openstack-keystone-2012.2.4
+/usr/share/doc/openstack-keystone-2012.2.4/LICENSE
+/usr/share/doc/openstack-keystone-2012.2.4/README.rst
+/usr/share/man/man1/keystone-all.1.gz
+/usr/share/man/man1/keystone-manage.1.gz
+/usr/share/openstack-keystone/
+/usr/share/openstack-keystone/openstack-keystone.upstart
+/usr/share/openstack-keystone/sample_data.sh
+/var/lib/keystone
+/var/log/keystone
+/var/run/keystone
